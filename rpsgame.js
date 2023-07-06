da24be3949125a47a3a5dce11a7e186b44e13f43
Jamie Newson <79523465+JamieNewson@users.noreply.github.com>
2023-07-06 20:52:10 +0100

    Random selection function created

--- rpsgame.js
+++ rpsgame.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+function getComputerSelection() {
+  //Define list of options, then select random option from list, then return selection
+  const options = ["rock", "paper", "scissors"];
+  const selection = options[Math.floor(Math.random() * options.length)];
+  return selection;
+}
+
+console.log(getComputerSelection());
