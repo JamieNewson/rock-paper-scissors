3d39afef8091c4b3da4a06efd3a3561046c1dfa9
Jamie Newson <79523465+JamieNewson@users.noreply.github.com>
2023-07-08 13:50:10 +0100

    Win Logic Complete Player receives feedback at end of game with results Score is counted, invalid arguments do not count towards round

--- rpsgame.js
+++ rpsgame.js
@@ -1,5 +1,9 @@
 const options = ["rock", "paper", "scissors"];
 
+let playerScore = 0;
+let computerScore = 0;
+let roundsPlayed = 0;
+
 function getComputerSelection() {
   //Define list of options, then select random option from list, then return selection
   const selection = options[Math.floor(Math.random() * options.length)];
@@ -11,27 +15,51 @@ function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   //Check if player input matches a valid response - 'rock','paper', or 'scissors'
   if (!options.includes(playerSelection)) {
-    console.log(
+    roundsPlayed--;
+    return console.log(
       playerSelection +
         " is not a playable option, please choose rock, paper or scissors."
     );
-    return;
   }
   //Check if player and computer input match first to remove it as a case
-  if (playerSelection === computerSelection) return "It's a draw!";
+  if (playerSelection === computerSelection) return "Draw";
   //Check through all cases and return relvent reponse (draw cases already taken care of)
   switch (playerSelection) {
     case "rock":
-      if (computerSelection === "paper") return "You lose, Paper beats Rock!";
-      return "You win, Rock beats Scissors!";
+      if (computerSelection === "paper") {
+        computerScore++;
+        return "Computer wins";
+      }
+      break;
     case "paper":
-      if (computerSelection === "scissors")
-        return "You lose, Scissors beats Paper!";
-      return "You win, Paper beats Rock!";
+      if (computerSelection === "scissors") {
+        computerScore++;
+        return "Computer wins";
+      }
+      break;
     case "scissors":
-      if (computerSelection === "rock") return "You lose, Rock beats Scissors!";
-      return "You win, Scissors beats Paper!";
+      if (computerSelection === "rock") {
+        computerScore++;
+        return "Computer wins";
+      }
+      break;
   }
+  playerScore++;
+  return "Player wins";
 }
 
-console.log(playRound("scissors", getComputerSelection()));
+function getWinCase() {
+  if (playerScore === computerScore) return "It's a draw.";
+  else if (playerScore >= computerScore) return "Player wins!";
+  else return "Computer wins!";
+}
+
+function game() {
+  for (roundsPlayed = 0; roundsPlayed < 5; roundsPlayed++) {
+    let playerInput = prompt("Enter your selection: ");
+    playRound(playerInput, getComputerSelection());
+  }
+  console.log(`${getWinCase()} ${playerScore} - ${computerScore}`);
+}
+
+game();
