2e7196114cc1e2cbc980575aeb84381e83183858
Jamie Newson <79523465+JamieNewson@users.noreply.github.com>
2023-07-15 16:36:57 +0100

    Full Game Complete - Javascript Logic - References to DOM elements - Usable UI elements - Simple, clean animation of buttons - Feedback to user on results

--- rpsgame.js
+++ rpsgame.js
@@ -3,72 +3,99 @@ const options = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
 let roundsPlayed = 0;
+let roundWinner = "";
+let previousWinner = "";
 
 function resetGame() {
   playerScore = 0;
   computerScore = 0;
   roundsPlayed = 0;
-  game();
+
+  updateGameText("Make your choice!", `${previousWinner} won the last game.`);
+  gameOver.classList.remove("gameOver");
 }
 
-function getComputerSelection() {
-  //Define list of options, then select random option from list, then return selection
-  const selection = options[Math.floor(Math.random() * options.length)];
-  return selection;
+function playRound(playerSelection) {
+  // Get the random computer selection & assign to variable
+  let computerSelection = options[Math.floor(Math.random() * options.length)];
+
+  // Check through all cases and add point to relevent winner, no points for a draw
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    playerScore++;
+    roundWinner = "player";
+  } else if (
+    (computerSelection === "rock" && playerSelection === "scissors") ||
+    (computerSelection === "paper" && playerSelection === "rock") ||
+    (computerSelection === "scissors" && playerSelection === "paper")
+  ) {
+    computerScore++;
+    roundWinner = "computer";
+  } else roundWinner = "tie";
+  roundWinnerOutcome(playerSelection, computerSelection);
+  if (playerScore === 5 || computerScore === 5) handleGameEnd();
 }
 
-function playRound(playerSelection, computerSelection) {
-  //Convert player input to be case-insensitive
-  playerSelection = playerSelection.toLowerCase();
-  //Check if player input matches a valid response - 'rock','paper', or 'scissors'
-  if (!options.includes(playerSelection)) {
-    roundsPlayed--;
-    return console.log(
-      playerSelection +
-        " is not a playable option, please choose rock, paper or scissors."
-    );
-  }
-  //Check if player and computer input match first to remove it as a case
-  if (playerSelection === computerSelection) return "Draw";
-  //Check through all cases and return relvent reponse (draw cases already taken care of)
-  switch (playerSelection) {
-    case "rock":
-      if (computerSelection === "paper") {
-        computerScore++;
-        return "Computer wins";
-      }
+function roundWinnerOutcome(playerSelection, computerSelection) {
+  playerSelection = formatText(playerSelection);
+  computerSelection = formatText(computerSelection);
+  let textToDisplay = "";
+  let subText = "Press your choice!";
+
+  switch (roundWinner) {
+    case "player":
+      textToDisplay = "You win!";
+      subText = `${playerSelection} beats ${computerSelection}`;
       break;
-    case "paper":
-      if (computerSelection === "scissors") {
-        computerScore++;
-        return "Computer wins";
-      }
+    case "computer":
+      textToDisplay = "You lose!";
+      subText = `${computerSelection} beats ${playerSelection}`;
       break;
-    case "scissors":
-      if (computerSelection === "rock") {
-        computerScore++;
-        return "Computer wins";
-      }
+    case "tie":
+      textToDisplay = "It's a tie!";
+      subText = `You both chose ${playerSelection}`;
       break;
   }
-  playerScore++;
-  return "Player wins";
+
+  updateGameText(textToDisplay, subText);
 }
 
-function getWinCase() {
-  if (playerScore === computerScore) return "It's a draw.";
-  else if (playerScore >= computerScore) return "Player wins!";
-  else return "Computer wins!";
+function updateGameText(textToDisplay, subText) {
+  roundResult.textContent = textToDisplay;
+  subDisplay.textContent = subText;
+  scoreDisplay.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
 }
 
-function game() {
-  for (roundsPlayed = 0; roundsPlayed < 5; roundsPlayed++) {
-    let playerInput = prompt("Enter your selection: ");
-    playRound(playerInput, getComputerSelection());
+function handleGameEnd() {
+  if (playerScore === 5) {
+    previousWinner = "You";
+    gameResult.textContent = `You won!`;
+  } else {
+    previousWinner = "Computer";
+    gameResult.textContent = "You lost!";
   }
-  console.log(`${getWinCase()} ${playerScore} - ${computerScore}`);
-  alert("Press 'Ok' to play again.");
-  resetGame();
+  gameOver.classList.add("gameOver");
 }
 
-game();
+function formatText(text) {
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+// Get references to objects in the DOM given their class
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
+const resetButton = document.querySelector(".reset");
+const roundResult = document.querySelector(".roundResult");
+const subDisplay = document.querySelector(".subText");
+const gameOver = document.querySelector(".overlay");
+const scoreDisplay = document.querySelector(".scoreDisplay");
+const gameResult = document.querySelector(".gameResult");
+
+rockButton.addEventListener("click", () => playRound("rock"));
+paperButton.addEventListener("click", () => playRound("paper"));
+scissorsButton.addEventListener("click", () => playRound("scissors"));
+resetButton.addEventListener("click", () => resetGame());
